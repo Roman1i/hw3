d95feefb793ae850b12608a3a456493fc3e4d6ce
Roman <igoshinr.estho@gmail.com>
2022-07-18 20:23:59 +0300

    019

---
+++ 019/Program.cs
@@ -0,0 +1,4 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000 || n > 99999) n = Convert.ToInt32(Console.ReadLine());
+if (Convert.ToString(n)[0] == Convert.ToString(n)[4] && Convert.ToString(n)[1] == Convert.ToString(n)[3]) Console.WriteLine("да");
+else Console.WriteLine("нет");
